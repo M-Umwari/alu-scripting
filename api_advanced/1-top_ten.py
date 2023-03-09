4ae289086b2da62aeb1ee83d1ea92bba5fdcec21
M-Umwari <m.umwari@alustudent.com>
2023-03-09 14:53:22 +0200

    1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
-"""get subscribers numbers function"""
 
+"""
+get top 10 hot posts function
+"""
 
 import json
 import requests
 import sys
 
 
-def number_of_subscribers(subreddit):
-    """get all subscribers"""
+def top_ten(subreddit):
+    """Prints the titles of the first 10 hot posts for a subreddit."""
     if len(sys.argv) < 2:
-        return 0
+        print(None)
     else:
-        url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+        url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
         headers = {"User-Agent": "Mozilla/5.0"}
         result = requests.get(url, headers=headers, allow_redirects=False)
         if result.status_code != 200:
-            return 0
-        body = json.loads(result.text)
-        return body["data"]["subscribers"]
-
+            print(None)
+        else:
+            data = json.loads(result.text)["data"]["children"]
+            for post in data[:10]:
+                print(post["data"]["title"])
